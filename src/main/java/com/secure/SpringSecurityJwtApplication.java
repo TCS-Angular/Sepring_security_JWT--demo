8330acd99148ec9a5297cc314e923216484f708d
ngactive99 <ngactive99@gmail.com>
2022-06-09 15:30:13 +0530

    added list of users who will be allowed to login

--- src/main/java/com/secure/SpringSecurityJwtApplication.java
+++ src/main/java/com/secure/SpringSecurityJwtApplication.java
@@ -1,10 +1,33 @@
 package com.secure;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.secure.entity.User;
+import com.secure.repository.UserRepository;
+
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
+	
+	@Autowired
+	private UserRepository repository;
+	@PostConstruct
+	public void initUsers() {
+		List<User> users = Stream.of(
+				new User(101,"admin","admin","admin@gmail.com"),
+				new User(102,"admin2","admin2","admin2@gmail.com"),
+				new User(103,"admin3","admin3","admin3@gmail.com"),
+				new User(104,"admin4","admin4","admin4@gmail.com")
+				).collect(Collectors.toList());
+		repository.saveAll(users);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtApplication.class, args);
