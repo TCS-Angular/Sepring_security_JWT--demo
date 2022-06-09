db2e72e75e21325e1016a2f56dbc877aed583f23
ngactive99 <ngactive99@gmail.com>
2022-06-09 15:07:04 +0530

    added entity and repo

---
+++ src/main/java/com/secure/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.secure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.secure.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
